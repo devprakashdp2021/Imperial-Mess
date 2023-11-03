1e33aa26f1ec2ad973cd3c27fc880c9aab6eebf5
Dev Prakash <devprakashdp2021@gmail.com>
2023-11-03 22:56:45 +0530

    react app created

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Slide from './component/Slide';
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1 style={{fontSize:50, margin:"0px"}}>Imperial Mess</h1>
+     <Slide />
+    </div>
+  );
+}
+
+export default App;
