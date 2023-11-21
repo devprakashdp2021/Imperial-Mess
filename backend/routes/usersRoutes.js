d820d95bb474fa37e21cad484ef2f7eebc900aa1
Himanshu Kumar <agarwalhimanshugaya@gmail.com>
2023-11-21 22:45:16 +0530

    add user block function

--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -2,7 +2,9 @@ const express=require("express");
 const {
     registerUser,
     authUser,
-    getuser
+    getuser,
+    Blockuser,
+    GetetallUser
 }=require("../controllers/userControllers");
 const authMiddleware=require("../middlewares/authMiddleware")
 const router=express.Router();
@@ -10,4 +12,6 @@ const router=express.Router();
 router.post("/",registerUser);
 router.post("/login",authUser);
 router.get("/get-current-user",authMiddleware,getuser)
+router.put("/block-user/:id",Blockuser);
+router.get("/get-all-user",GetetallUser);
 module.exports=router;
--- frontend/src/components/ViewAllStudents.js
+++ frontend/src/components/ViewAllStudents.js
@@ -1,33 +1,60 @@
 import { SearchOutlined } from '@ant-design/icons';
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState,useEffect } from 'react';
 import Highlighter from 'react-highlight-words';
-import { Button, Input, Space, Table } from 'antd';
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    gsuiteid: "abc@gmail.com",
-  },
-  {
-    key: '2',
-    name: 'Joe Black',
-    gsuiteid: "abc@gmail.com",
-  },
-  {
-    key: '3',
-    name: 'Jim Green',
-    gsuiteid: "abc@gmail.com",
-  },
-  {
-    key: '4',
-    name: 'Jim Red',
-    gsuiteid: "abc@gmail.com",
-  },
-];
+import { Button, Input, Space, Table ,message} from 'antd';
+import { HideLoading, ShowLoading } from '../redux/loadersSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { Blockuser, Getalluser } from '../apicalls/users';
+
 const ViewAllStudents = (props) => {
   const [searchText, setSearchText] = useState('');
   const [searchedColumn, setSearchedColumn] = useState('');
   const searchInput = useRef(null);
+  const [isLoading,setLoading]=useState(true);
+  const[data,setData]=useState({});
+  const dispatch = useDispatch();
+  async function fetchalluser (){
+    try {
+      // console.log(user);
+      dispatch(ShowLoading());
+      let  response = await Getalluser();
+          if(response.success){
+              setData(response.data.map((item,index)=>({
+                  key:index+1,
+                  name:item.name,
+                  gsuiteid:item.gsuiteid,
+                  id:item._id,
+                  type:item.isActive
+              })) 
+              )
+              // console.log(response.data)
+          }else{
+              message.error(response.message);
+          }
+          setLoading(false);
+          dispatch(HideLoading());
+      } catch (error) {
+          dispatch(HideLoading());
+          message.error(error.message);
+      }
+  }
+   const handleBlock=async(event)=>{
+    const id=event.id;
+    
+    console.log(event);
+    try{
+       dispatch(ShowLoading);
+       let response=await Blockuser(id);
+       if(response.success){
+        await fetchalluser()
+        message.info(`${event.name} has been ${event.type?"Blocked":"Unblocked"}`);
+       }else{
+        message.error('An error occurred while Blocking')
+       }
+    }catch(error){
+      message.error(error.message);
+    }
+  }
   const handleSearch = (selectedKeys, confirm, dataIndex) => {
     confirm();
     setSearchText(selectedKeys[0]);
@@ -154,12 +181,18 @@ const ViewAllStudents = (props) => {
       render: (text, record) => (
         <>
           {props.buttonFor === "chiefWarden" && (
-            <Button onClick={() => console.log(record)}>{"Block"}</Button>
+            <Button onClick={()=>handleBlock(record)}>{record.type===true?"Block":"Unblock"}</Button>
           )}
         </>
       ),
     },
   ];
+  useEffect(() => {
+    fetchalluser();
+  }, []);
+  if(isLoading){
+    return <div> Loading....</div>;
+  }
   return <Table columns={columns} dataSource={data} pagination={false}/>;
 };
 export default ViewAllStudents;
